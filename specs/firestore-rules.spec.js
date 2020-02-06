a71cc0cf83ac8a24f89b8908d6aa6a65d092095c
Laurent Shala <laurentshala@icloud.com>
2020-02-05 22:21:18 -0500

    implement firestore-rules spec

---
+++ specs/firestore-rules.spec.js
@@ -0,0 +1,194 @@
+const { setup, teardown } = require('./helpers');
+const { assertFails, assertSucceeds } = require('@firebase/testing');
+
+const mockData = {
+    '/Notification/someUserId': {},
+    '/groups/someGroupId': {},
+    '/reports/someUserId': {},
+    '/topics/someTopic': {},
+    'users/someUserId': {}
+  };  
+
+describe('Notification collection rules', () => {
+    var database;
+    var collection;
+
+    afterAll(async () => {
+        await teardown();
+    });
+
+    test('allow reading to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('Notification');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('allow reading to authenticated users', async () => {
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('Notification');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('deny writing to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('Notification');
+
+        await expect(collection.add({})).toDeny();
+    })
+
+    test('allow writing to authenticated users', async () => {     
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('Notification');
+
+        await expect(collection.add({})).toAllow();
+    });
+});
+
+describe('groups collection rules', () => {
+    var database;
+    var collection;
+
+    afterAll(async () => {
+        await teardown();
+    });
+
+    test('allow reading to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('groups');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('allow reading to authenticated users', async () => {
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('groups');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('deny writing to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('groups');
+
+        await expect(collection.add({})).toDeny();
+    })
+
+    test('allow writing to authenticated users ', async () => {     
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('groups');
+
+        await expect(collection.add({})).toAllow();
+    });
+});
+
+describe('reports collection rules', () => {
+    var database;
+    var collection;
+
+    afterAll(async () => {
+        await teardown();
+    });
+
+    test('allow reading to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('reports');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('allow reading to authenticated users', async () => {
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('reports');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('deny writing to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('reports');
+
+        await expect(collection.add({})).toDeny();
+    })
+
+    test('allow writing to authenticated users ', async () => {     
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('reports');
+
+        await expect(collection.add({})).toAllow();
+    });
+});
+
+describe('topics collection rules', () => {
+    var database;
+    var collection;
+
+    afterAll(async () => {
+        await teardown();
+    });
+
+    test('allow reading to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('topics');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('allow reading to authenticated users', async () => {
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('deny writing to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('topics');
+
+        await expect(collection.add({})).toDeny();
+    })
+
+    test('allow writing to authenticated users ', async () => {     
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.add({})).toAllow();
+    });
+});
+
+describe('users collection rules', () => {
+    var database;
+    var collection;
+
+    afterAll(async () => {
+        await teardown();
+    });
+    test('allow reading to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('users');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('allow reading to authenticated users', async () => {
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('users');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('deny writing to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('users');
+
+        await expect(collection.add({})).toDeny();
+    })
+
+    test('allow writing to authenticated users ', async () => {     
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('users');
+
+        await expect(collection.add({})).toAllow();
+    });
+});
