86fcca0a55daafed0b92dd93181aff481533dc70
Laurent Shala <laurentshala@icloud.com>
2020-02-15 17:51:55 -0500

    add unit tests for new user collection rules

--- specs/usersCollectionRules.spec.js
+++ specs/usersCollectionRules.spec.js
@@ -2,11 +2,15 @@ const { setup, teardown } = require('./helpers');
 const { assertFails, assertSucceeds } = require('@firebase/testing');
 
 const mockData = {
-    '/Notification/someUserId': {},
-    '/groups/someGroupId': {},
-    '/reports/someUserId': {},
-    '/topics/someTopic': {},
-    'users/someUserId': {}
+    'users/userId1': {
+        rank: 'Worker Bee'
+    },
+    'users/userId2': {
+        rank: 'Queen Bee'
+    },
+    'users/userId3': {
+        rank: 'Admin'
+    }
 };  
 
 describe('users collection rules', () => {
@@ -16,31 +20,95 @@ describe('users collection rules', () => {
     afterAll(async () => {
         await teardown();
     });
+
+    // MARK: - Read
+
     test('allow reading to unauthenticated users', async () => {
         database = await setup();
         collection = database.collection('users');
 
-        await expect(collection.get()).toAllow();
+        await expect(collection.doc('userId1').get()).toAllow();
     });
 
     test('allow reading to authenticated users', async () => {
         database = await setup({ uid: 'userAuthToken' }, mockData);
         collection = database.collection('users');
 
-        await expect(collection.get()).toAllow();
+        await expect(collection.doc('userId1').get()).toAllow();
     });
 
-    test('deny writing to unauthenticated users', async () => {
+    // MARK: - Create
+
+    test('deny creating to unauthenticated users', async () => {
         database = await setup();
         collection = database.collection('users');
 
-        await expect(collection.add({})).toDeny();
+        await expect(collection.add({ 'userId4': {} })).toDeny();
     })
 
-    test('allow writing to authenticated users ', async () => {     
+    test('allow creating to authenticated users ', async () => {     
         database = await setup({ uid: 'userAuthToken' }, mockData);
         collection = database.collection('users');
 
-        await expect(collection.add({})).toAllow();
+        await expect(collection.add({ 'userId4': {} })).toAllow();
+    });
+
+    // MARK: - Update
+
+    test('deny updating to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('users');
+
+        await expect(collection.doc('userId1').set({role: "Queen Bee"})).toDeny();
+    });
+
+    test('deny updating to a user that is not the document owner', async () => {
+        database = await setup({ uid: 'userId2' }, mockData);
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').set({rank: "Queen Bee"})).toDeny();
+    });
+
+    test('deny updating to a user that is the document owner but tries to change the rank', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').set({ rank: 'Queen Bee' })).toDeny();
+    });
+
+    test('allow updating to a user that is the document owner', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').set({ name: 'Billy', rank: 'Worker Bee' })).toAllow();
+    });
+
+    test('allow admin to update a different users data', async () => {
+        database = await setup({ uid: 'userId3' }, mockData);
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').set({ name: 'Billy', rank: 'Worker Bee' })).toAllow();
+    });
+
+    // MARK: - Delete
+
+    test('deny deleting to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').delete()).toDeny();
+    });
+
+    test('deny deleting to a user that is not the document owner', async () => {
+        database = await setup({ uid: 'userId2' }, mockData);
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').delete()).toDeny();
+    });
+
+    test('allow deleting to a user that is the document owner', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').delete()).toAllow();
+    });
+
+    test('allow admin to delete any user', async () => {
+        database = await setup({ uid: 'userId3' }, mockData);
+        collection = database.collection('users');
+        await expect(collection.doc('userId1').delete()).toAllow();
     });
 });
