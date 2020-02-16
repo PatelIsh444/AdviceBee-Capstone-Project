a25ce05ac872666d5327d36876246c3d32196814
Laurent Shala <laurentshala@icloud.com>
2020-02-16 17:45:02 -0500

    move existing topics collection tests to new file

---
+++ specs/topicsCollectionRules.spec.js
@@ -0,0 +1,47 @@
+const { setup, teardown } = require('./helpers');
+const { assertFails, assertSucceeds } = require('@firebase/testing');
+
+const mockData = {
+    '/Notification/someUserId': {},
+    '/groups/someGroupId': {},
+    '/reports/someUserId': {},
+    '/topics/someTopic': {},
+    'users/someUserId': {}
+};
+
+describe('topics collection rules', () => {
+    var database;
+    var collection;
+
+    afterAll(async () => {
+        await teardown();
+    });
+
+    test('allow reading to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('topics');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('allow reading to authenticated users', async () => {
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.get()).toAllow();
+    });
+
+    test('deny writing to unauthenticated users', async () => {
+        database = await setup();
+        collection = database.collection('topics');
+
+        await expect(collection.add({})).toDeny();
+    })
+
+    test('allow writing to authenticated users ', async () => {     
+        database = await setup({ uid: 'userAuthToken' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.add({})).toAllow();
+    });
+});
