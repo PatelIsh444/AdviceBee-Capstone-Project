cc3ffcd8e763e394f01d42b2851b5d6c8acf0606
Laurent Shala <laurentshala@icloud.com>
2020-02-16 21:41:24 -0500

    add topic collection unit tests

--- specs/topicsCollectionRules.spec.js
+++ specs/topicsCollectionRules.spec.js
@@ -2,12 +2,27 @@ const { setup, teardown } = require('./helpers');
 const { assertFails, assertSucceeds } = require('@firebase/testing');
 
 const mockData = {
-    '/Notification/someUserId': {},
-    '/groups/someGroupId': {},
-    '/reports/someUserId': {},
-    '/topics/someTopic': {},
-    'users/someUserId': {}
-};
+    'topics/General/topicQuestions/questionId1': {
+        createdBy: 'userId1',
+        numOfResponses: 0
+    },
+    'topics/General/topicQuestions/questionId2': {
+        createdBy: 'userId1',
+        numOfResponses: 1
+    },
+    'users/userId1': {
+        rank: 'Worker Bee',
+        myTopics: ['General']
+    },
+    'users/userId2': {
+        rank: 'Admin',
+        myTopics: ['Art']
+    },
+    'users/userId3': {
+        rank: 'Worker Bee',
+        myTopics: ['General']
+    }
+};  
 
 describe('topics collection rules', () => {
     var database;
@@ -17,31 +32,137 @@ describe('topics collection rules', () => {
         await teardown();
     });
 
+    // MARK: - Read
+
     test('allow reading to unauthenticated users', async () => {
         database = await setup();
         collection = database.collection('topics');
 
         await expect(collection.get()).toAllow();
+        await expect(collection.doc('General').get()).toAllow();
+        await expect(collection.doc('General').collection('topicQuestions').get()).toAllow();
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').get()).toAllow();
     });
 
     test('allow reading to authenticated users', async () => {
-        database = await setup({ uid: 'userAuthToken' }, mockData);
+        database = await setup({ uid: 'userId1' }, mockData);
         collection = database.collection('topics');
 
         await expect(collection.get()).toAllow();
+        await expect(collection.doc('General').get()).toAllow();
+        await expect(collection.doc('General').collection('topicQuestions').get()).toAllow();
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').get()).toAllow();
     });
 
-    test('deny writing to unauthenticated users', async () => {
-        database = await setup();
+    // MARK: - Create
+
+    test('allow creating a new topic to admins', async () => {
+        database = await setup({ uid: 'userId2' }, mockData);
         collection = database.collection('topics');
 
-        await expect(collection.add({})).toDeny();
-    })
-
-    test('allow writing to authenticated users ', async () => {     
-        database = await setup({ uid: 'userAuthToken' }, mockData);
-        collection = database.collection('topics');
-
-        await expect(collection.add({})).toAllow();
+        await expect(collection.add({ 'Martial Arts': {} })).toAllow();
     });
+
+    test('deny creating a new topics to non-admins', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.add({ 'Martial Arts': {} })).toDeny();
+    });
+
+    test('allow creating a new topic quest to members', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').add({})).toAllow();
+    });
+
+    test('allow creating a new topic quest to members if user is admin', async () => {
+        database = await setup({ uid: 'userId2' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').add({})).toAllow();
+    });
+
+    test('deny creating a new topic quest to non-members', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('Art').collection('topicQuestions').add({})).toDeny();
+    });
+
+    // MARK: - Update
+
+    test('allow updating a question if user is the creator', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').set({ 'question': 'A different title'})).toAllow();
+    });
+
+    test('allow updating a question if user an admin', async () => {
+        database = await setup({ uid: 'userId2' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').set({ 'question': 'A different title'})).toAllow();
+    });
+
+    test('deny updating a question if user is not the creator', async () => {
+        database = await setup({ uid: 'userId3' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').set({ 'question': 'A different title'})).toDeny();
+    });
+
+    test('deny updating a question if user is the creator but question has responses', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId2').set({ 'question': 'A different title'})).toDeny();
+    });
+
+    test('deny updating a question if user is not the creator and question has responses', async () => {
+        database = await setup({ uid: 'userId3' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId2').set({ 'question': 'A different title'})).toDeny();
+    });
+
+    // MARK: - Delete
+
+    test('allow deleting a topic if user an admin', async () => {
+        database = await setup({ uid: 'userId2' }, mockData);
+        collection = database.collection('topics');
+        
+        await expect(collection.doc('General').delete()).toAllow();
+    });
+
+    test('deny deleting a topic if user not an admin', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('topics');
+        
+        await expect(collection.doc('General').delete()).toDeny();
+    });
+
+    test('allow deleting a question if user is the creator', async () => {
+        database = await setup({ uid: 'userId1' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').delete()).toAllow();
+    });
+
+    test('allow deleting a question if user an admin', async () => {
+        database = await setup({ uid: 'userId2' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').delete()).toAllow();
+    });
+
+    test('deny deleting a question if user is not the creator', async () => {
+        database = await setup({ uid: 'userId3' }, mockData);
+        collection = database.collection('topics');
+
+        await expect(collection.doc('General').collection('topicQuestions').doc('questionId1').delete()).toDeny();
+    });
+
 });
