25cb78c28ef4ff436fe601d30037f0d91ced4904
Claudio Miraka <gs5791@wayne.edu>
2020-03-02 18:32:19 -0500

    adding profile picture for search bar for user and log in page overflow fixed

--- lib/pages/searchUser.dart
+++ lib/pages/searchUser.dart
@@ -1,29 +1,31 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../newProfile.dart';
 
-class searchUser extends SearchDelegate<String>{
+class searchUser extends SearchDelegate<String> {
+  List<DocumentSnapshot> suggestions = [];
 
-  List<DocumentSnapshot> suggestions=[] ;
-
-
-  getUserData(s)async{
-    await Firestore.instance.collection("users").where("displayId", whereIn: s).getDocuments()
-    .then((docs){
+  getUserData(s) async {
+    await Firestore.instance
+        .collection("users")
+        .where("displayId", whereIn: s)
+        .getDocuments()
+        .then((docs) {
       print(docs);
     });
-    
   }
 
   @override
   List<Widget> buildActions(BuildContext context) {
     return <Widget>[
       IconButton(
-        icon: Icon(Icons.clear),onPressed:(){
-          query="";
-      })
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
     ];
   }
 
@@ -43,10 +45,11 @@ class searchUser extends SearchDelegate<String>{
 
   @override
   Widget buildResults(BuildContext context) {
-    Stream<QuerySnapshot> stream = Firestore.instance.collection('users').snapshots();
+    Stream<QuerySnapshot> stream =
+        Firestore.instance.collection('users').snapshots();
     return StreamBuilder(
       stream: stream,
-      builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (!snapshot.hasData) {
           return Center(
             child: CircularProgressIndicator(
@@ -54,7 +57,10 @@ class searchUser extends SearchDelegate<String>{
             ),
           );
         } else {
-          suggestions=snapshot.data.documents.where((d) => d["displayName"].toString().toLowerCase().contains(query)).toList();
+          suggestions = snapshot.data.documents
+              .where((d) =>
+                  d["displayName"].toString().toLowerCase().contains(query))
+              .toList();
 
           return SuggestionList(
             query: query,
@@ -63,16 +69,15 @@ class searchUser extends SearchDelegate<String>{
         }
       },
     );
-
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-
-    Stream<QuerySnapshot> stream = Firestore.instance.collection('users').snapshots();
+    Stream<QuerySnapshot> stream =
+        Firestore.instance.collection('users').snapshots();
     return StreamBuilder(
       stream: stream,
-      builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (!snapshot.hasData) {
           return Center(
             child: CircularProgressIndicator(
@@ -80,7 +85,10 @@ class searchUser extends SearchDelegate<String>{
             ),
           );
         } else {
-          suggestions=snapshot.data.documents.where((d) => d["displayName"].toString().toLowerCase().contains(query)).toList();
+          suggestions = snapshot.data.documents
+              .where((d) =>
+                  d["displayName"].toString().toLowerCase().contains(query))
+              .toList();
 
           return SuggestionList(
             query: query,
@@ -94,6 +102,7 @@ class searchUser extends SearchDelegate<String>{
 
 class SuggestionList extends StatelessWidget {
   const SuggestionList({this.suggestions, this.query});
+
   final List<DocumentSnapshot> suggestions;
   final String query;
 
@@ -101,10 +110,37 @@ class SuggestionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: suggestions.length,
-      itemBuilder: (context,index ) {
-        if (query == null) {return Container();}
+      itemBuilder: (context, index) {
+        if (query == null) {
+          return Container();
+        }
+        String avatar = suggestions.elementAt(index)["profilePicURL"];
         return ListTile(
-          leading: Icon(Icons.people),
+          leading: Material(
+            child: avatar != null
+                ? CachedNetworkImage(
+                    placeholder: (context, url) => Container(
+                      child: CircularProgressIndicator(
+                        strokeWidth: 1.0,
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
+                      ),
+                      width: 50.0,
+                      height: 50.0,
+                      padding: EdgeInsets.all(15.0),
+                    ),
+                    imageUrl: avatar,
+                    width: 50.0,
+                    height: 50.0,
+                    fit: BoxFit.cover,
+                  )
+                : Icon(
+                    Icons.account_circle,
+                    size: 50.0,
+                    color: Colors.grey,
+                  ),
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            clipBehavior: Clip.hardEdge,
+          ),
           title: RichText(
             text: TextSpan(
               text: suggestions.elementAt(index)["displayName"],
@@ -114,12 +150,13 @@ class SuggestionList extends StatelessWidget {
               ),
             ),
           ),
-          onTap: () {Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    UserDetailsPage(suggestions.elementAt(index).documentID),
-              ));
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      UserDetailsPage(suggestions.elementAt(index).documentID),
+                ));
           },
         );
       },
