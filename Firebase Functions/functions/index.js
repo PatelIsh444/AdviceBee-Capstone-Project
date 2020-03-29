a596197029902ec4f097b4dea681f7504004de4f
Laurent Shala <laurentshala@icloud.com>
2020-03-29 15:56:21 -0400

    properly import FieldValue

--- Firebase Functions/functions/index.js
+++ Firebase Functions/functions/index.js
@@ -4,6 +4,8 @@
 
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const FieldValue = require('firebase-admin').firestore.FieldValue;
+
 admin.initializeApp()
 
 /*
