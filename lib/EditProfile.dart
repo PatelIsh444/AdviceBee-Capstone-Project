b2fa0cae2f4caf0b305e2db687dbcfe1b6e87c2f
Mustafa Ahmed <fo8197@wayne.edu>
2020-04-06 01:32:11 -0400

    App - Log out button should be within the button of the profile page #132 Fixed

--- lib/EditProfile.dart
+++ lib/EditProfile.dart
@@ -52,22 +52,6 @@ class EditProfilePageState extends State<EditProfilePage> {
 
   //Method to signout
   //Deallocate all routes and end user's session
-  Future<void> _SignOut(BuildContext context) async {
-    try {
-      final auth = AuthProvider.of(context);
-      await auth.SignOut();
-      setUserLastAccess();
-      //Destroy all navigation stacks
-      Navigator.of(context)
-          .pushNamedAndRemoveUntil(MyApp.id, (Route<dynamic> route) => false);
-    } catch (e) {
-      print(e.toString());
-    }
-    setState(() {
-      CurrentUser = null;
-    });
-
-  }
 
   Future<void> setData() async {
     userRef.get().then((DocumentSnapshot ds) {
@@ -161,39 +145,6 @@ class EditProfilePageState extends State<EditProfilePage> {
     );
   }
 
-  Widget buildSignOutButton() {
-    return Padding(
-      //padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-      padding: EdgeInsets.only(left: 10, top: 8, bottom: 30),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: GestureDetector(
-              onTap: () => _SignOut(context),
-              child: Container(
-                height: 40.0,
-                decoration: BoxDecoration(
-                  borderRadius: new BorderRadius.all(new Radius.circular(20.0)),
-                  color: Colors.teal[300],
-                ),
-                child: Center(
-                  child: Text(
-                    "SIGN OUT",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(width: 10.0),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -260,7 +211,6 @@ class EditProfilePageState extends State<EditProfilePage> {
               ),
             ),
             buildEditButton(),
-            buildSignOutButton(),
           ],
         ),
       ),
