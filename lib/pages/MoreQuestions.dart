cd098397dcd0cb152f85a6cccafae624328261f4
Mustafa Ahmed <fo8197@wayne.edu>
2020-04-20 22:26:51 -0400

    Fixing buy more questions page

--- lib/pages/MoreQuestions.dart
+++ lib/pages/MoreQuestions.dart
@@ -1,17 +1,207 @@
+//import 'package:credit_card/credit_card_form.dart';
+//import 'package:credit_card/credit_card_model.dart';
+//import 'package:credit_card/credit_card_widget.dart';
+//import 'package:flushbar/flushbar.dart';
+//import 'package:flutter/material.dart';
+//import 'package:stripe_payment/stripe_payment.dart';
+//import 'package:v0/pages/PaymentConfirm.dart';
+//
+//import 'package:v0/services/PaymentService.dart';
+//import 'package:v0/utils/commonFunctions.dart';
+//
+//import '../Dashboard.dart';
+//import '../MoreMenu.dart';
+//import '../QuestionPage.dart';
+//
+//class BuyMoreQuestions extends StatefulWidget {
+//  @override
+//  _BuyMoreQuestionsState createState() => new _BuyMoreQuestionsState();
+//}
+//
+//class _BuyMoreQuestionsState extends State<BuyMoreQuestions> {
+//  Token _paymentToken;
+//  PaymentMethod _paymentMethod;
+//  String _error = 'Pending...';
+//  Source _source;
+//
+//  var paymentService = new PaymentService();
+//
+//  ScrollController _controller = ScrollController();
+//
+//  String cardNumber = '';
+//  String expiryDate = '';
+//  String cardHolderName = '';
+//  String cvvCode = '';
+//  bool isCvvFocused = false;
+//  int cardMonth = 12;
+//  int cardYear = 21;
+//
+//  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+//  GlobalKey key = GlobalKey();
+//
+//  @override
+//  initState() {
+//    super.initState();
+//    StripePayment.setOptions(StripeOptions(
+//        publishableKey: "pk_test_9GSIOVrscIpH7WlH9p5donv100k7On42UV",
+//        merchantId: "AdviceBee",
+//        androidPayMode: 'test'));
+//    StripePayment.createSourceWithParams(SourceParams(
+//      type: 'AdviceBeePro',
+//      amount: 0099,
+//      currency: 'usd',
+//      returnURL: 'example://stripe-redirect',
+//    )).then((source) {
+//      _scaffoldKey.currentState.showSnackBar(
+//          SnackBar(content: Text('Received ${_source.sourceId}')));
+//      setState(() {
+//        _source = source;
+//      });
+//    }).catchError(setError);
+//  }
+//
+//  void setError(dynamic error) {
+//    Flushbar(
+//      title: _error.toString(),
+//      message: " ",
+//      duration: Duration(seconds: 5),
+//      backgroundColor: Colors.teal,
+//    ).show(context);
+////    _scaffoldKey.currentState
+////        .showSnackBar(SnackBar(content: Text(_error.toString())));
+//    setState(() {
+//      _error = error.toString();
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        automaticallyImplyLeading: false,
+//        centerTitle: true,
+//        title: Text('Online Store'),
+//        leading: MaterialButton(
+//          minWidth: MediaQuery.of(context).size.width / 5,
+//          onPressed: () {
+//            Navigator.pop(context);
+//            if (CurrentUser.isNotGuest) {
+//            } else {
+//              guestUserSignInMessage(context);
+//            }
+//          },
+//          child: Icon(
+//            Icons.arrow_back,
+//            color: Colors.white,
+//            size: 30,
+//          ),
+//        ),
+//      ),
+//      body: Column(
+//        children: [
+//          ListView(
+//            children: <Widget>[
+//              CreditCardWidget(
+//                cardNumber: cardNumber,
+//                expiryDate: expiryDate,
+//                cardHolderName: cardHolderName,
+//                cvvCode: cvvCode,
+//                showBackView:
+//                isCvvFocused, //true when you want to show cvv(back) view
+//              ),
+//              CreditCardForm(
+//                onCreditCardModelChange: onCreditCardModelChange,
+//              ),
+//              RaisedButton(
+//                color: Colors.green,
+//                child: Text(
+//                  "Buy More Questions",
+//                  style: TextStyle(
+//                    color: Colors.white,
+//                    fontSize: 25,
+//                  ),
+//                ),
+//                onPressed: () {
+//                  cardMonth = int.parse(expiryDate.split('/')[0]);
+//                  cardYear = int.parse(expiryDate.split('/')[1]);
+//                  final CreditCard paymentCard = CreditCard(
+//                    number: cardNumber,
+//                    expMonth: cardMonth,
+//                    expYear: cardYear,
+//                  );
+//                  StripePayment.createTokenWithCard(
+//                    paymentCard,
+//                  ).then((token) {
+//                    _scaffoldKey.currentState.showSnackBar(
+//                        SnackBar(content: Text('Received ${token.tokenId}')));
+//                    setState(() {
+//                      _paymentToken = token;
+//                    });
+//                    paymentService.addUserCard(token);
+//                    Navigator.push(
+//                        context,
+//                        MaterialPageRoute(
+//                          builder: (context) => PaymentConfirm(),
+//                        ));
+//                  }).catchError(setError);
+//                },
+//              ),
+//            ],
+//          ),
+//        ]
+//      ),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: () {
+//          if (CurrentUser.isNotGuest) {
+//            Navigator.push(
+//                context,
+//                MaterialPageRoute(
+//                    builder: (context) => postQuestion(null, null) //AddPost(),
+//                    ));
+//          } else {
+//            guestUserSignInMessage(context);
+//          }
+//        },
+//        heroTag: "my2PostsHero",
+//        tooltip: 'Increment',
+//        child: CircleAvatar(
+//          child: Image.asset(
+//            'images/addPostIcon4.png',
+//          ),
+//          maxRadius: 12,
+//        ),
+//      ),
+//      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//      bottomNavigationBar: globalNavigationBar(2, context, key, false),
+//    );
+//  }
+//
+//  void onCreditCardModelChange(CreditCardModel creditCardModel) {
+//    setState(() {
+//      cardNumber = creditCardModel.cardNumber;
+//      expiryDate = creditCardModel.expiryDate;
+//      cardHolderName = creditCardModel.cardHolderName;
+//      cvvCode = creditCardModel.cvvCode;
+//      isCvvFocused = creditCardModel.isCvvFocused;
+//    });
+//  }
+//}
+//
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:credit_card/credit_card_form.dart';
 import 'package:credit_card/credit_card_model.dart';
 import 'package:credit_card/credit_card_widget.dart';
-import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:stripe_payment/stripe_payment.dart';
-import 'package:v0/pages/PaymentConfirm.dart';
 
 import 'package:v0/services/PaymentService.dart';
 import 'package:v0/utils/commonFunctions.dart';
 
 import '../Dashboard.dart';
-import '../MoreMenu.dart';
-import '../QuestionPage.dart';
+import '../Profile.dart';
+import 'PaymentConfirm.dart';
 
 class BuyMoreQuestions extends StatefulWidget {
   @override
@@ -21,8 +211,7 @@ class BuyMoreQuestions extends StatefulWidget {
 class _BuyMoreQuestionsState extends State<BuyMoreQuestions> {
   Token _paymentToken;
   PaymentMethod _paymentMethod;
-  String _error = 'Pending...';
-  Source _source;
+  String _error = 'Pending.';
 
   var paymentService = new PaymentService();
 
@@ -37,7 +226,8 @@ class _BuyMoreQuestionsState extends State<BuyMoreQuestions> {
   int cardYear = 21;
 
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  GlobalKey key = GlobalKey();
+
+  get currentTab => null;
 
   @override
   initState() {
@@ -46,29 +236,11 @@ class _BuyMoreQuestionsState extends State<BuyMoreQuestions> {
         publishableKey: "pk_test_9GSIOVrscIpH7WlH9p5donv100k7On42UV",
         merchantId: "AdviceBee",
         androidPayMode: 'test'));
-    StripePayment.createSourceWithParams(SourceParams(
-      type: 'AdviceBeePro',
-      amount: 0099,
-      currency: 'usd',
-      returnURL: 'example://stripe-redirect',
-    )).then((source) {
-      _scaffoldKey.currentState.showSnackBar(
-          SnackBar(content: Text('Received ${_source.sourceId}')));
-      setState(() {
-        _source = source;
-      });
-    }).catchError(setError);
   }
 
   void setError(dynamic error) {
-    Flushbar(
-      title: _error.toString(),
-      message: " ",
-      duration: Duration(seconds: 5),
-      backgroundColor: Colors.teal,
-    ).show(context);
-//    _scaffoldKey.currentState
-//        .showSnackBar(SnackBar(content: Text(_error.toString())));
+    _scaffoldKey.currentState
+        .showSnackBar(SnackBar(content: Text(_error.toString())));
     setState(() {
       _error = error.toString();
     });
@@ -81,11 +253,15 @@ class _BuyMoreQuestionsState extends State<BuyMoreQuestions> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
-        title: Text('Online Store'),
+        title: Text('Payment History'),
         leading: MaterialButton(
           minWidth: MediaQuery.of(context).size.width / 5,
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProfilePage(),
+                ));
             if (CurrentUser.isNotGuest) {
             } else {
               guestUserSignInMessage(context);
@@ -100,27 +276,21 @@ class _BuyMoreQuestionsState extends State<BuyMoreQuestions> {
       ),
       body: ListView(
         children: <Widget>[
-          Expanded(
-            child: CreditCardWidget(
-              cardNumber: cardNumber,
-              expiryDate: expiryDate,
-              cardHolderName: cardHolderName,
-              cvvCode: cvvCode,
-              showBackView:
-                  isCvvFocused, //true when you want to show cvv(back) view
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: CreditCardForm(
-                onCreditCardModelChange: onCreditCardModelChange,
-              ),
-            ),
-          ),
           Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
+                CreditCardWidget(
+                  cardNumber: cardNumber,
+                  expiryDate: expiryDate,
+                  cardHolderName: cardHolderName,
+                  cvvCode: cvvCode,
+                  showBackView:
+                  isCvvFocused, //true when you want to show cvv(back) view
+                ),
+                CreditCardForm(
+                  onCreditCardModelChange: onCreditCardModelChange,
+                ),
                 RaisedButton(
                   color: Colors.green,
                   child: Text(
@@ -160,31 +330,67 @@ class _BuyMoreQuestionsState extends State<BuyMoreQuestions> {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (CurrentUser.isNotGuest) {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => postQuestion(null, null) //AddPost(),
-                    ));
-          } else {
-            guestUserSignInMessage(context);
-          }
-        },
-        heroTag: "my2PostsHero",
-        tooltip: 'Increment',
-        child: CircleAvatar(
-          child: Image.asset(
-            'images/addPostIcon4.png',
-          ),
-          maxRadius: 12,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: globalNavigationBar(2, context, key, false),
     );
   }
+
+  Widget _generateReportsCategoryReason() {
+    List<TableRow> history = List();
+    String reasons = "";
+    String priceCard = "";
+    String lastFourCard = "";
+    return StreamBuilder(
+        stream: Firestore.instance.collection('cards').document(CurrentUser.userID).collection('purchaseHistory').orderBy('date').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            snapshot.data.documents.forEach((e) {
+              reasons = (e.data["itemPurchased"]);
+              priceCard = (e.data["itemCost"]);
+              lastFourCard = (e.data["lastFour"]);
+              history.add(TableRow(
+                  children: [
+                    Text(
+                      reasons,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          color: Colors.black
+                      ),
+                    ),
+                    Text(
+                      priceCard,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          color: Colors.black
+                      ),
+                    ),
+                    Text(
+                      lastFourCard,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          color: Colors.black
+                      ),
+                    ),
+                  ]));
+            });
+            return Container(
+              padding: EdgeInsets.all(5),
+              child: Table(
+                defaultColumnWidth: FlexColumnWidth(1.0),
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                border: TableBorder.all(color: Colors.black12),
+                children: history,
+              ),
+            );
+          }
+          else {
+            return Text("Loading report category...");
+          }
+        }
+    );
+  }
+
 
   void onCreditCardModelChange(CreditCardModel creditCardModel) {
     setState(() {
